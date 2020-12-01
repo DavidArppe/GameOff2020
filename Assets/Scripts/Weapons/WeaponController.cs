caa37160c026d93992ccf1e9117b4f4acca8d20a
David Arppe <david.arppe@gmail.com>
2020-12-01 12:12:34 -0500

    Player Aiming

---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Transform gunBarrelTrans;
+    public Transform gunPivotTrans;
+
+    void Update()
+    {
+        AimWeapon();    
+    }
+
+    void AimWeapon()
+    {
+        Vector3 target = Camera.main.transform.position + Camera.main.transform.forward * 10000.0f;
+
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f)), out RaycastHit info))
+        {
+            target = info.point;
+        }
+
+        gunPivotTrans.LookAt(gunPivotTrans.position + Vector3.ProjectOnPlane(target - gunPivotTrans.position, transform.up), transform.up);
+        gunBarrelTrans.LookAt(gunBarrelTrans.position + Vector3.ProjectOnPlane(target - gunBarrelTrans.position, gunPivotTrans.right), transform.up);
+    }
+}
